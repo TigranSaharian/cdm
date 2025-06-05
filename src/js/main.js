175f35d62bf6c2593807ba1fdb8538281884d15d
TigranSaharyan <tigran1205@gmail.com>
2025-06-06 01:27:40 +0400

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function ($) {
+
+    'use strict';
+    /************** Mixitup (Filter Projects) *********************/
+    $('.filter-gallery').mixitup({
+        effects: ['fade', 'grayscale'],
+        easing: 'snap',
+        transitionSpeed: 400
+    });
+
+});
